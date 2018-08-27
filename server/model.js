d2f554b4c57016910e9f9d7b0a8d52f825e2516d
Lee <545360100@qq.com>
2018-08-27 22:20:25 +0800

    建立聊天数据库模型

--- server/model.js
+++ server/model.js
@@ -1,30 +1,34 @@
 const DB_URL = "mongodb://localhost:27017/imooc-chat";
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  DB_URL,
-  { useNewUrlParser: true }
-);
+mongoose.connect(DB_URL, { useNewUrlParser: true });
 mongoose.connection.on("connected", () => {
   console.log("mongo connect success");
 });
 
 const models = {
   user: {
-    'user': { type: String, require: true },
-    'pwd': { type: String, require: true },
-    'type': { type: String, require: true },
+    user: { type: String, require: true },
+    pwd: { type: String, require: true },
+    type: { type: String, require: true },
     // 头像
-    'avatar': { type: String },
+    avatar: { type: String },
     // 个人简介
-    'desc': { type: String },
-    'title': { type: String },
+    desc: { type: String },
+    title: { type: String },
     // boss的字段
-    'company': { type: String },
-    'money': { type: String }
+    company: { type: String },
+    money: { type: String }
   },
   // 聊天
-  chat: {}
+  chat: {
+    chatid: { type: String, require: true },
+    from: { type: String, require: true },
+    to: { type: String, require: true },
+    read: { type: Boolean, require: true, default: false },
+    content: { type: String, require: true, default: "" },
+    create_time: { type: Number, default: new Date().getTime() }
+  }
 };
 
 for (let m in models) {
