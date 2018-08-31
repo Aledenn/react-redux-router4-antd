e42084b07dcf6666855bbefdf1b327eab237257c
Lee <545360100@qq.com>
2018-08-31 14:24:01 +0800

    完成redux没有react-redux

--- src/index.js
+++ src/index.js
@@ -1,79 +1,33 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-import { createStore,applyMiddleware, compose} from 'redux';
-import { counter} from './index.redux';
-import thunk from 'redux-thunk';
-import {Provider} from 'react-redux'
-import {BrowserRouter,Route,Link,
-        Redirect,Switch
-} from 'react-router-dom'
+import { createStore } from "./myRedux";
+// import { createStore } from "redux";
 
-const store = createStore(counter,compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension?window.devToolsExtension():()=>{}
-)
-)
+function counter(state = 0, action) {
+  console.log(action);
+  switch (action.type) {
+    case "加机关枪":
+      return state + 1;
+    case "减机关枪":
+      return state - 1;
+    default:
+      return 10;
+  }
+}
+// 创建store
+const store = createStore(counter);
 
-function Erying() {
-    return <h2>二营</h2>
+const init = store.getState();
+console.log(`一开始有${init}把`);
+
+function listener() {
+  const current = store.getState();
+  console.log(`现在有机枪${current}把`);
 }
 
-function Qibinglian() {
-    return <h2>骑兵连</h2>
-}
+// 订阅，每次state修改，都会执行listener
+store.subscribe(listener);
 
-class Test extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        console.log(this.props);
-        // 可实现页面跳转
-        // this.props.history.push('/')
-        return <h2>测试组件{this.props.location.pathname}</h2>
-    }
-}
-
-// provider组件在应用最外层。传入store即可。只用一次
-
-ReactDom.render(
-    // Provider包裹最外层
-<Provider store={store}>
-    <BrowserRouter>
-    <Switch>
-
-        
-    </Switch>
-    <div>
-    <ul>
-        <li>
-            <Link to="/">一营</Link>
-        </li>
-        <li>
-            <Link to="/erying">二营</Link>
-        </li>
-        <li>
-            <Link to="/qibinglian">骑兵连</Link>
-        </li>
-    </ul>
-    {/* <Redirect to='/qibinglian'></Redirect> */}
-    {/* Switch只命中第一个抓取得 */}
-    <Switch>
-        {/* exact完全匹配 否则App会和别的内容一起出现*/}
-        <Route path='/' exact component={App}></Route>
-        <Route path='/erying' component={Erying}></Route>
-        {/* <Route path='/:location' component={Test}></Route> */}
-        <Route path='/qibinglian' component={Qibinglian}></Route>
-        <Route patch='/:location' component={Test}></Route>
-
-    </Switch>
-
-    </div>
- 
-</BrowserRouter>
-</Provider>    
-,
-    document.getElementById('root'))
-
-
+// 提交状态改变的申请
+store.dispatch({ type: "加机关枪" });
+store.dispatch({ type: "加机关枪" });
+store.dispatch({ type: "加机关枪" });
+store.dispatch({ type: "减机关枪" });
